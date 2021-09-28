a7e6735a67c2deaa76c979c01035e1b2aa55bc0a
Nick-713 <pereiranicolas713@gmail.com>
2021-09-27 22:23:24 -0300

    estrutura pronta

---
+++ public/main.js
@@ -0,0 +1,9 @@
+function showHome() {
+  document.getElementById("main").style.display= 'grid';
+  document.getElementById("profile").style.display= 'none';
+}
+
+function showProfile() {
+  document.getElementById("main").style.display= 'none';
+  document.getElementById("profile").style.display= 'grid';
+}
